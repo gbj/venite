ef18a38c4878e98f8881157e5efe0e5e0c140c85
Greg Johnston <greg.johnston@gmail.com>
2020-05-06 07:19:19 -0400

    LDF back to CommonJS so it can be used with Node

--- ldf/src/editing/change.ts
+++ ldf/src/editing/change.ts
@@ -1,11 +1,13 @@
 export class Change {
   user?: string;  // Username; Overridden by the server when it broadcasts it
+  path: string;
   op: 'insert' | 'delete' | 'set';
   pos: number;
   length: number;
   value: string;
 
-  constructor(op: 'insert' | 'delete' | 'set', pos: number, length: number, value: string) {
+  constructor(path : string, op: 'insert' | 'delete' | 'set', pos: number, length: number, value: string) {
+    this.path = path;
     this.op = op;
     this.pos = pos;
     this.length = length;
