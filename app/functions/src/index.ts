78ac93b36394309472a7c618c981743fc7d400ea
Greg Johnston <greg.johnston@gmail.com>
2020-06-15 20:34:05 -0400

    Create User Profiles on user creation

--- app/functions/src/index.ts
+++ app/functions/src/index.ts
@@ -1,8 +1,17 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
- export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
- });
+/** Save the user's info in a UserProfile collection on first login */
+export const saveUserProfile = functions.auth.user().onCreate((user) => {
+    admin.firestore().collection('Users').doc(user.uid).set({
+        uid: user.uid,
+        ... ( user.displayName && { displayName : user.displayName }),
+        ... ( user.photoURL && { photoURL : user.photoURL }),
+        organizations: []
+    });
+});
+
+/** Save the user's info in a UserProfile collection on first login */
+export const deleteUserProfile = functions.auth.user().onDelete((user) => {
+    admin.firestore().collection('Users').doc(user.uid).delete();
+});
