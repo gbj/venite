afdf9c5243a823657523fc02a7fb12fccb9e1a13
Greg Johnston <greg.johnston@gmail.com>
2020-07-06 21:12:53 -0400

    Module for PlatformService

---
+++ app/projects/platform/src/lib/platform.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { PLATFORM_ID, Inject } from '@angular/core';
+import { isPlatformServer } from '@angular/common';
+import { Platform } from '@ionic/angular';
+import { PlatformServiceInterface } from 'service-api';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlatformService implements PlatformServiceInterface {
+
+  constructor(
+    private platform : Platform,
+    @Inject(PLATFORM_ID) private ngPlatform: Object
+  ) {
+  }
+
+  is(p : string) : boolean {
+    if(isPlatformServer(this.ngPlatform) && p !== 'server') {
+      return false;
+    } else if(p == 'server') {
+      return isPlatformServer(this.ngPlatform);
+    } else {
+      return this.platform.is(p as "ios" | "ipad" | "iphone" | "android" | "phablet" | "tablet" | "cordova" | "capacitor" | "electron" | "pwa" | "mobile" | "mobileweb" | "desktop" | "hybrid");
+    }
+  }
+
+  ready() : Promise<string> {
+    if(isPlatformServer(this.ngPlatform)) {
+      return Promise.resolve('server');
+    } else {
+      return this.platform.ready();
+    }
+  }
+}
