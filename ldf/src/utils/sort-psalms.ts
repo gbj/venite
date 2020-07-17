90241bdcd2cb67d28b40de3dd7e642d0cdfd118d
Greg Johnston <greg.johnston@gmail.com>
2020-07-17 07:26:40 -0400

    Error handling for sortPsalms

--- ldf/src/utils/sort-psalms.ts
+++ ldf/src/utils/sort-psalms.ts
@@ -8,13 +8,21 @@ export function sortPsalms(a: Psalm, b: Psalm): number {
     return Number(firstNumber) || 0;
   }
 
-  const sortByNum = Number(a?.metadata?.number) - Number(b?.metadata?.number);
-  let sortByVerse: number | undefined = undefined;
-  // if psalms are same number, sort by
-  if (sortByNum === 0) {
-    const firstVerseOfA = firstVerse(a),
-      firstVerseOfB = firstVerse(b);
-    sortByVerse = firstVerseOfA - firstVerseOfB;
+  let result : number;
+  try {
+    const sortByNum = Number(a?.metadata?.number) - Number(b?.metadata?.number);
+    let sortByVerse: number | undefined = undefined;
+    // if psalms are same number, sort by
+    if (sortByNum === 0) {
+      const firstVerseOfA = firstVerse(a),
+        firstVerseOfB = firstVerse(b);
+      sortByVerse = firstVerseOfA - firstVerseOfB;
+    }
+    result = sortByVerse || sortByNum;
+  } catch(e) {
+    console.warn("sortPsalms was unable to determine which of these two psalms should be placed first: ", a, b)
+    console.warn(e);
+    result = 0;
   }
-  return sortByVerse || sortByNum;
+  return result;
 }
