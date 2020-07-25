215b296dfa2de1d11a46a2f50f581df8ead9e74e
Greg Johnston <greg.johnston@gmail.com>
2020-07-24 21:26:56 -0400

    Allow flexible date/day to be included in Heading

--- ldf/src/heading.ts
+++ ldf/src/heading.ts
@@ -7,6 +7,13 @@ export class Heading extends LiturgicalDocument {
     /** `level` roughly corresponds to HTML headers H1, H2, etc.
      * Higher numbers are 'lower' in a tree */
     level?: number;
+
+    /** What kind of value to display
+     * If `text`, displays plain text stored in `value`
+     * If `day`, displays name of `LiturgicalDay` stored in `day`
+     * If `date`, displays localized version of `Date` determined by `dateFromYMDString(day.date)`
+     */
+    type: 'text' | 'day' | 'date';
   };
 
   /** Contains the text of the heading */
