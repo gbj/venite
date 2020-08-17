ad07c4612e95aa62cfe84f3e7671c96b820755e6
Greg Johnston <greg.johnston@gmail.com>
2020-08-16 20:54:17 -0400

    Include DarkMode

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -34,7 +34,7 @@ import { PlatformService } from '@venite/ng-platform';
 import { BIBLE_SERVICE, PLATFORM_SERVICE, LOCAL_STORAGE, PREFERENCES_SERVICE, AUTH_SERVICE, CALENDAR_SERVICE, LECTIONARY_SERVICE, DOCUMENT_SERVICE, CANTICLE_TABLE_SERVICE } from '@venite/ng-service-api';
 import { LocalStorageService } from '@venite/ng-localstorage';
 import { PreferencesService } from './preferences/preferences.service';
-//import { DarkmodeModule } from '@venite/ng-darkmode';
+import { DarkmodeModule } from '@venite/ng-darkmode';
 import { PrayMenuModule } from '@venite/ng-pray-menu';
 import { AuthService } from './auth/auth.service';
 import { LectionaryService } from './services/lectionary.service';
@@ -71,8 +71,8 @@ import { CanticleTableService } from './services/canticle-table.service';
         PlatformService,
         LocalStorageService
       ]
-    }),*/
-    /*DarkmodeModule.forRoot({
+    }),
+    DarkmodeModule.forRoot({
       providers: [
         PreferencesService
       ]
