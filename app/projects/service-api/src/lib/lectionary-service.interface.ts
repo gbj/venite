6ff65f9903e00b3773278f2f2df295e8eceb7748
Greg Johnston <greg.johnston@gmail.com>
2020-10-08 22:47:30 -0400

    Enable alternate year lectionary

--- app/projects/service-api/src/lib/lectionary-service.interface.ts
+++ app/projects/service-api/src/lib/lectionary-service.interface.ts
@@ -2,5 +2,5 @@ import { LiturgicalDay, LectionaryEntry } from '@venite/ldf';
 import { Observable } from 'rxjs';
 
 export interface LectionaryServiceInterface {
-  getReadings : (day : LiturgicalDay, lectionaryName : string, readingType : string) => Observable<LectionaryEntry[]>;
+  getReadings : (day : LiturgicalDay, lectionaryName : string, readingType : string, alternateYear : boolean) => Observable<LectionaryEntry[]>;
 }
