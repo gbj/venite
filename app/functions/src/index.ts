39862b99f00bee616af1629529ee0c48a08d303e
Greg Johnston <greg.johnston@gmail.com>
2020-10-12 23:15:59 -0400

    Hymn text and image APIs into Cloud Functions

--- app/functions/src/index.ts
+++ app/functions/src/index.ts
@@ -1,22 +1,6 @@
 import * as functions from 'firebase-functions';
 import { getBibleText } from '@venite/bible-api';
-//import * as admin from 'firebase-admin';
-
-// These were having issues firing, so handling from client side instead
-
-/** Save the user's info in a UserProfile collection on first login */
-/*export const saveUserProfile = functions.auth.user().onCreate(async (user) => {
-    await admin.firestore().collection('Users').doc(user.uid).set({
-        uid: user.uid,
-        ... ( user.displayName && { displayName : user.displayName }),
-        ... ( user.photoURL && { photoURL : user.photoURL })
-    });
-});*/
-
-/** Save the user's info in a UserProfile collection on first login */
-/*export const deleteUserProfile = functions.auth.user().onDelete(async (user) => {
-    await admin.firestore().collection('Users').doc(user.uid).delete();
-});*/
+import { loadText, loadScore } from '@venite/hymnal-api';
 
 export const bible = functions.https.onRequest(async (request, response) => {
   response.set('Access-Control-Allow-Origin', '*'); // CORS allowed
@@ -37,5 +21,49 @@ export const bible = functions.https.onRequest(async (request, response) => {
     }
   } else {
     response.status(400).send("'Citation' and 'Version' parameters are required.")
+  }
+});
+
+export const hymnText = functions.https.onRequest(async (request, response) => {
+  response.set('Access-Control-Allow-Origin', '*'); // CORS allowed
+
+  const url = request.query?.url?.toString();
+
+  if(url) {
+    try {
+      const text = await loadText(url);
+      if(text?.length > 0) {
+        response.set('Cache-Control', 'public, max-age=2592000'); // allow caching for 2,592,000 seconds = 30 days
+        response.status(200).send(text);
+      } else {
+        response.status(404).send(`Text at '${URL}' not found.`)
+      }
+    } catch(e) {
+      response.status(400).send(e.toString());
+    }
+  } else {
+    response.status(400).send("'URL' parameter is required.")
+  }
+});
+
+export const hymnImages = functions.https.onRequest(async (request, response) => {
+  response.set('Access-Control-Allow-Origin', '*'); // CORS allowed
+
+  const url = request.query?.url?.toString();
+
+  if(url) {
+    try {
+      const urls = await loadScore(url);
+      if(urls?.length > 0) {
+        response.set('Cache-Control', 'public, max-age=2592000'); // allow caching for 2,592,000 seconds = 30 days
+        response.status(200).send(urls);
+      } else {
+        response.status(404).send(`Page scans at '${URL}' not found.`)
+      }
+    } catch(e) {
+      response.status(400).send(e.toString());
+    }
+  } else {
+    response.status(400).send("'URL' parameter is required.")
   }
 });
