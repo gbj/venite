01584d5111e66a287c88d52da96dbe421080d546
Greg Johnston <greg.johnston@gmail.com>
2020-10-12 23:06:12 -0400

    Abstract simple HTTPS/HTML parsing functions

---
+++ http/index.ts
@@ -0,0 +1,20 @@
+import { parse, HTMLElement } from 'node-html-parser';
+import * as https from 'https';
+
+export { HTMLElement } from 'node-html-parser';
+
+export async function requestHTML(url : string) : Promise<HTMLElement> {
+  const raw = await httpsGet(url);
+  return parse(raw);
+}
+
+export async function httpsGet(url : string, headers : any = {}) : Promise<string> {
+  return new Promise((resolve, reject) => {
+    https.get(url, { headers }, (res: any) => {
+      res.setEncoding('utf8');
+      let body = '';
+      res.on('data', (chunk : string) => body += chunk);
+      res.on('end', () => resolve(body));
+    }).on('error', reject);
+  });
+};
