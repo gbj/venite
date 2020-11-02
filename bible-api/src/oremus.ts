dac0f0eaf281c65a831435cfcb607ec2029a8cc1
Greg Johnston <greg.johnston@gmail.com>
2020-11-01 20:46:01 -0500

    Add newlines in e.g., NRSVAE Apocrypha

--- bible-api/src/oremus.ts
+++ bible-api/src/oremus.ts
@@ -66,6 +66,8 @@ export function parseOremusResponse(citation : string, textEl : HTMLElement) : (
         } else if(child instanceof HTMLElement && child.tagName == 'a') {
         } else if(child instanceof HTMLElement && child.classNames.includes('sc')) {
           verseTexts.push(child.text.replace(/\n$/, ' ').replace(/\s+/g, ' ').toUpperCase());
+        } else if(child instanceof HTMLElement && child.tagName == 'br') {
+          verseTexts.push('\n');
         } else if(child.text?.length < 20 && child.text?.endsWith('-->')) {
         } else {
           verseTexts.push(child.text.replace(/\n$/, ' ').replace(/\s+/g, ' '));
