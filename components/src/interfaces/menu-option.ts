f833363af6bd3161aba019e7575953a3e2568744
Greg Johnston <greg.johnston@gmail.com>
2020-11-28 13:34:47 -0500

    Adders for POTP, Eucharistic Prayer, Offertory Sentences

--- components/src/interfaces/menu-option.ts
+++ components/src/interfaces/menu-option.ts
@@ -7,5 +7,5 @@ export class MenuOption {
   icon: () => JSX.Element;
   template?: LiturgicalDocument[];
   hidden?: boolean;
-  needsMoreInfo?: 'psalm' | 'canticle' | 'lectionary' | 'hymn' | 'liturgy' | 'invitatory' | 'image' | 'eucharistic-prayer' | 'response';
+  needsMoreInfo?: 'psalm' | 'canticle' | 'lectionary' | 'hymn' | 'liturgy' | 'invitatory' | 'image' | 'category' | 'slug' | 'response';
 }
