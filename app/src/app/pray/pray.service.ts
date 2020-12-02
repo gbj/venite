5ec460c8b0b5c2682d8ba2550dca8594e25548e1
Greg Johnston <greg.johnston@gmail.com>
2020-12-02 14:19:07 -0500

    Fix insertGloria behavior

--- app/src/app/pray/pray.service.ts
+++ app/src/app/pray/pray.service.ts
@@ -247,7 +247,7 @@ export class PrayService {
           ...doc.metadata,
           omit_antiphon: doc?.metadata?.omit_antiphon ?? docBase?.metadata?.omit_antiphon,
           // also omit Gloria Patri if `insertGloria` === 'false'
-          omit_gloria: doc?.metadata?.omit_gloria ?? docBase?.metadata?.omit_gloria ?? (docBase?.style === 'psalm' && Boolean(prefs['insertGloria'] == 'false')),
+          omit_gloria: (docBase?.style === 'psalm' && Boolean(prefs['insertGloria'] == 'false')) ?? doc?.metadata?.omit_gloria ?? docBase?.metadata?.omit_gloria,
           omit_response: doc?.metadata?.omit_response ?? docBase?.metadata?.omit_response,
           changeable: doc?.metadata?.changeable ?? docBase?.metadata?.changeable
         }
