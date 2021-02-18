072b487c248f5eb51840cf0d84d564dc88da76ee
Greg Johnston <greg.johnston@gmail.com>
2021-02-18 12:12:02 -0500

    About page

---
+++ app/src/app/about/about.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'venite-about',
+  templateUrl: './about.page.html',
+  styleUrls: ['./about.page.scss'],
+})
+export class AboutPage implements OnInit {
+  tab : string = 'about'; // 'about'|'support'|'privacy';
+
+  constructor(
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    this.route.fragment.subscribe((fragment: string) => {
+      if(['about', 'support', 'privacy'].includes(fragment)) {
+        this.tab = fragment;
+      }
+    });
+  }
+
+}
--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -42,6 +42,10 @@ const routes: Routes = [
   {
     path: 'template',
     loadChildren: () => import('./template/template.module').then( m => m.TemplatePageModule)
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then( m => m.AboutPageModule)
   }
 ];
 
