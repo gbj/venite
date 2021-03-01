3ab7606a8494e5cdf93fcfc06ae677797509c60a
Greg Johnston <greg.johnston@gmail.com>
2021-03-01 14:23:04 -0500

    Set up correct feast transferring

--- app/src/app/services/calendar.service.ts
+++ app/src/app/services/calendar.service.ts
@@ -1,12 +1,11 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
-import { Observable, combineLatest, of } from 'rxjs';
-import { map, tap, switchMap, filter, startWith, shareReplay } from 'rxjs/operators';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable, combineLatest, of, from } from 'rxjs';
+import { map, switchMap, filter, tap, startWith } from 'rxjs/operators';
 
-import { HolyDay, Kalendar, Liturgy, LiturgicalDay, LiturgicalDocument, LiturgicalWeek, LiturgicalWeekIndex, ProperLiturgy, addOneDay, dateFromYMD, dateToYMD, liturgicalWeek, liturgicalDay } from '@venite/ldf';
+import { HolyDay, Kalendar, Liturgy, LiturgicalDay, LiturgicalDocument, LiturgicalWeek, LiturgicalWeekIndex, ProperLiturgy, addOneDay, dateFromYMD, dateToYMD, liturgicalWeek, liturgicalDay, transferredFeast } from '@venite/ldf';
 import { CalendarServiceInterface } from '@venite/ng-service-api';
 import { HttpClient } from '@angular/common/http';
-import { LoadingController } from '@ionic/angular';
 
 import WEEKS from '../../offline/weeks.json';
 import KALENDAR from '../../offline/kalendar.json';
@@ -14,7 +13,7 @@ import KALENDAR from '../../offline/kalendar.json';
 @Injectable({
   providedIn: 'root'
 })
-export class CalendarService {
+export class CalendarService implements CalendarServiceInterface {
 
   constructor(private readonly afs: AngularFirestore, private http : HttpClient) { }
 
@@ -121,38 +120,65 @@ export class CalendarService {
   /** Find `HolyDay`s connected to either a date or a slug */
    addHolyDays(day : LiturgicalDay, vigil : boolean) : Observable<LiturgicalDay> {
     // generate query from the `LiturgicalDay`
-    const [y, m, d] = day.date.split('-'),
-          date = dateFromYMD(y, m, d),
-          isSunday = date.getDay() === 0,
-          observedDate = vigil ? addOneDay(date) : date,
-          observedM = observedDate.getMonth()+1,
-          observedD = observedDate.getDate(),
-          feasts = this.findFeastDays(day.kalendar, `${observedM}/${observedD}`),
-          specials = this.findSpecialDays(day.kalendar, day.slug);
+    const kalendar = day?.kalendar || 'bcp1979',
+      [y, m, d] = day.date.split('-'),
+      date = dateFromYMD(y, m, d),
+      isSunday = date.getDay() === 0,
+      observedDate = vigil ? addOneDay(date) : date,
+      observedM = observedDate.getMonth()+1,
+      observedD = observedDate.getDate(),
+      feasts$ = this.findFeastDays(kalendar, `${observedM}/${observedD}`),
+      specials$ = this.findSpecialDays(kalendar, day.slug);
 
     // Thanksgiving Day
     const isNovember = date.getMonth() === 10, // January is 0, Feb 1, etc., so Sept is 8 
       isThursday = date.getDay() === 4, // Sunday is 0, Monday is 1
       nthWeekOfMonth = Math.ceil(date.getDate() / 7),
-      thanksgiving = isNovember && isThursday && nthWeekOfMonth == 4 ? this.findSpecialDays(day.kalendar, 'thanksgiving-day') : of([]);
+      thanksgiving$ = isNovember && isThursday && nthWeekOfMonth === 4 ? this.findSpecialDays(kalendar, 'thanksgiving-day') : of([] as HolyDay[]);
+    // Transferred feasts
+    const allHolyDays = KALENDAR[kalendar]
+        .filter(hd => !hd?.type?.rank || hd?.type?.rank > 2)
+        // original holy days defaulted to rank 3, so if no rank present opt for 3
+        .map(hd => hd?.type?.rank ? hd : {...hd, type: { rank: 3 }}),
+      // add transferred feast days
+      transferred$ : Observable<HolyDay | null> = from(transferredFeast(
+        async (dfd : Date) => {
+          const week = await this.findWeek('bcp1979', liturgicalWeek(dfd)).toPromise();
+          return liturgicalDay(dfd, kalendar, false, week[0]);
+        },
+        (slug: string) => Promise.resolve(allHolyDays.find(dd => dd.slug === slug)),
+        (dfd : Date) => Promise.resolve(allHolyDays.find(dd => dd.mmdd === `${dfd.getMonth()+1}/${dfd.getDate()}`)),
+        date
+      )).pipe(
+        startWith(null)
+      );
 
-    return combineLatest(feasts, specials, thanksgiving)
+    return combineLatest([feasts$, specials$, thanksgiving$, transferred$])
       .pipe(
         // OR together the feasts and specials
-        map(([feasts, specials, thanksgiving]) => feasts.concat(specials).concat(thanksgiving)),
+        map(([feasts, specials, thanksgiving, transferred]) => feasts
+          .concat(transferred ? [transferred] : [])
+          .concat(specials)
+          .concat(thanksgiving)
+        ),
         // remove black-letter days that fall on a major feast
         map(holydays => {
-          const highestHolyDayRank = Math.max(... holydays.map(holyday => holyday.type?.rank));
+          const highestHolyDayRank = Math.max(... holydays.map(holyday => holyday.type?.rank ?? 3));
           if(highestHolyDayRank >= 4) {
-            return holydays.filter(holyday => holyday.octave || !holyday.type?.rank || holyday.type?.rank >= highestHolyDayRank);
+            holydays = holydays.filter(holyday => holyday.octave || !holyday.type?.rank || holyday.type?.rank >= highestHolyDayRank);
           } else if(isSunday) {
-            return holydays.filter(holyday => holyday.octave || !holyday.type?.rank || holyday.type?.rank >= 4);
-          } else {
-            return holydays;
+            holydays = holydays.filter(holyday => holyday.octave || !holyday.type?.rank || holyday.type?.rank >= 4);
           }
-        }),
-        // incorporate them into the `LiturgicalDay`
-        map(holydays => day.addHolyDays(holydays))
+    
+          // if some calendar other than bcp1979, filter out duplicate slugs
+          if(kalendar !== "bcp1979") {
+            const preferredCalendarSlugs = holydays.filter(holyday => holyday.kalendar === kalendar && holyday.slug).map(holyday => holyday.slug);
+            holydays = holydays.filter(holyday => holyday.kalendar === kalendar || !preferredCalendarSlugs.includes(holyday.slug));
+          }
+    
+          // incorporate them into the `LiturgicalDay`
+          return day.addHolyDays(holydays);
+        })
       );
   }
 
