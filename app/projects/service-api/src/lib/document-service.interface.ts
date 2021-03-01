b035adbb8b262af6022f4a78b4eb38251b8d87d3
Greg Johnston <greg.johnston@gmail.com>
2021-03-01 14:04:59 -0500

    Update DocumentService API to include disableOffline and bulletinMode params

--- app/projects/service-api/src/lib/document-service.interface.ts
+++ app/projects/service-api/src/lib/document-service.interface.ts
@@ -2,9 +2,9 @@ import { Observable } from 'rxjs';
 import { LiturgicalColor, LiturgicalDocument, Liturgy } from '@venite/ldf';
 
 export interface DocumentServiceInterface {
-  findDocumentsBySlug : (slug : string, language : string, versions : string[] | undefined) => Observable<LiturgicalDocument[]>;
+  findDocumentsBySlug : (slug : string, language : string, versions : string[] | undefined, disableOffline? : boolean, bulletinMode? : boolean) => Observable<LiturgicalDocument[]>;
 
-  findDocumentsByCategory : (category : string[], language : string, versions : string[]) => Observable<LiturgicalDocument[]>;
+  findDocumentsByCategory : (category : string[], language : string, versions : string[], disableOffline? : boolean, bulletinMode? : boolean) => Observable<LiturgicalDocument[]>;
 
   getLiturgyOptions : (language : string, version : string) => Observable<Liturgy[]>;
 
