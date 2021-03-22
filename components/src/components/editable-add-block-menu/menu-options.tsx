4238eae81068c1242547de9e9a336d9f9f7ef03d
Greg Johnston <greg.johnston@gmail.com>
2021-03-22 10:37:21 -0400

    Lord's Prayer adder

--- components/src/components/editable-add-block-menu/menu-options.tsx
+++ components/src/components/editable-add-block-menu/menu-options.tsx
@@ -1,5 +1,5 @@
 import { h } from '@stencil/core';
-import { LiturgicalDocument, Text, BibleReading, Meditation, Heading, ResponsivePrayer, Rubric, Psalm, Refrain, Liturgy, Image } from '@venite/ldf';
+import { LiturgicalDocument, Text, BibleReading, Option, Meditation, Heading, ResponsivePrayer, Rubric, Psalm, Refrain, Liturgy, Image } from '@venite/ldf';
 import { MenuOption } from '../../interfaces/menu-option';
 
 export const MENU : MenuOption[] = [
@@ -234,6 +234,56 @@ export const MENU : MenuOption[] = [
     needsMoreInfo: 'category'
   },
   {
+    label: 'our-father',
+    section: ['Prayer'],
+    /* SVG is Font Awesome 'fa-praying-hands' */
+    icon: () => (<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="praying-hands" class="svg-inline--fa fa-praying-hands fa-w-20" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="currentColor" d="M272 191.91c-17.6 0-32 14.4-32 32v80c0 8.84-7.16 16-16 16s-16-7.16-16-16v-76.55c0-17.39 4.72-34.47 13.69-49.39l77.75-129.59c9.09-15.16 4.19-34.81-10.97-43.91-14.45-8.67-32.72-4.3-42.3 9.21-.2.23-.62.21-.79.48l-117.26 175.9C117.56 205.9 112 224.31 112 243.29v80.23l-90.12 30.04A31.974 31.974 0 0 0 0 383.91v96c0 10.82 8.52 32 32 32 2.69 0 5.41-.34 8.06-1.03l179.19-46.62C269.16 449.99 304 403.8 304 351.91v-128c0-17.6-14.4-32-32-32zm346.12 161.73L528 323.6v-80.23c0-18.98-5.56-37.39-16.12-53.23L394.62 14.25c-.18-.27-.59-.24-.79-.48-9.58-13.51-27.85-17.88-42.3-9.21-15.16 9.09-20.06 28.75-10.97 43.91l77.75 129.59c8.97 14.92 13.69 32 13.69 49.39V304c0 8.84-7.16 16-16 16s-16-7.16-16-16v-80c0-17.6-14.4-32-32-32s-32 14.4-32 32v128c0 51.89 34.84 98.08 84.75 112.34l179.19 46.62c2.66.69 5.38 1.03 8.06 1.03 23.48 0 32-21.18 32-32v-96c0-13.77-8.81-25.99-21.88-30.35z"></path></svg>),
+    template: [ new Option(    {
+      "value": [
+        new Text({
+          "type": "text",
+          "version_label": "Traditional",
+          "display_format": "unison",
+          "category": [],
+          "value": [
+            "Our Father, who art in heaven,\n\thallowed be thy Name,\n\tthy kingdom come,\n\tthy will be done,\n\ton earth as it is in heaven.\nGive us this day our daily bread.\nAnd forgive us our trespasses,\n\tas we forgive those\n\twho trespass against us.\nAnd lead us not into temptation,\n\tbut deliver us from evil.\nFor thine is the kingdom,\n\tand the power, and the glory,\n\tfor ever and ever."
+          ],
+          "label": "The Lord’s Prayer",
+          "source": {
+            "citation": "",
+            "api": "",
+            "source": ""
+          },
+          "hidden": false,
+          "style": "prayer"
+        }),
+        new Text({
+          "style": "prayer",
+          "category": [],
+          "display_format": "unison",
+          "source": {
+            "citation": "",
+            "source": "",
+            "api": ""
+          },
+          "value": [
+            "Our Father in heaven,\n\thallowed be your Name,\n\tyour kingdom come,\n\tyour will be done,\n\ton earth as in heaven.\nGive us today our daily bread.\nForgive us our sins,\n\tas we forgive those\n\twho sin against us.\nSave us from the time of trial,\n\tand deliver us from evil.\nFor the kingdom, the power,\n\tand the glory are yours,\n\tnow and for ever."
+          ],
+          "label": "The Lord’s Prayer",
+          "type": "text",
+          "hidden": false,
+          "version_label": "Contemporary"
+        })
+      ],
+      "metadata": {
+        "editor_selected": 0,
+        "selected": 0
+      },
+      "hidden": false,
+      "type": "option"
+    }) ]
+  },
+  {
     label: 'closing-sentence',
     section: ['Liturgy'],
     /* SVG is Font Awesome 'fa-comments' */
@@ -293,7 +343,7 @@ export const MENU : MenuOption[] = [
     label: 'psalm-template',
     section: ['For Creating Templates'],
     icon: () => (<svg version="1.0" width="1065.000000pt" height="1280.000000pt" viewBox="0 0 1065.000000 1280.000000" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg">  <g transform="translate(0.000000,1280.000000) scale(0.100000,-0.100000)" fill="#000000" stroke="none">    <path d="M 2020 12790 C 1990 12785 1918 12777 1860 12770 C 1623 12744 672.669 12273.564 523 12002 C 316.287 11626.935 -405 9634.932 1000 5598.932 C 1837 3196.932 3560 15 3560 10 C 3560 -3 5211 101 5487 166 C 5827 246 6072 380 6161 534 C 6189 583 9162 2764 9755 3855 C 10133 4550 10348 5103 10493 5760 C 10635 6399 10683 7382 10609 8140 C 10541 8842 10287 9670 9914 10400 C 9873 10480 9840 10548 9840 10551 C 9840 10555 9870 10580 9907 10607 C 10066 10723 10186 10867 10280 11051 L 10333 11156 L 9885 11810 C 9639 12170 9429 12473 9420 12484 C 9403 12501 9399 12499 9314 12418 C 9075 12191 8794 12040 8305 11874 C 7675 11659 7197 11560 6790 11560 C 6350 11560 5829 11648 5170 11835 C 4824 11933 4715 11976 3895 12342 C 3247 12632 2966 12722 2576 12765 C 2499 12774 2402 12785 2362 12790 C 2278 12802 2100 12801 2020 12790 Z M 2236.679 11071.163 C 2314.679 11050.163 3527.302 10462.707 3527.302 10450.707 C 3527.302 10427.707 2669.082 8800 2657.082 8800 C 2651.082 8800 2030.847 9568.173 2000 10084.133 C 1982.133 10382.989 2124.679 11102.163 2236.679 11071.163 Z M 4424 10170 C 4424 10170 5001 10049.652 5000 10027.652 C 5000 9996.652 3551.769 7151.685 3536.769 7151.685 C 3526.769 7151.685 3234 7780 3215 7841 C 3209 7860 4408 10172 4424 10170 Z M 6063 9808 C 6141 9796 6762.015 9806 6764.015 9800 C 6767.015 9793 4380.708 5056.389 4374.708 5058.389 C 4369.708 5060.389 4120 5839 4084 5918 L 4020 6060 C 4020 6060 5983 9800 6000 9800 C 6011 9800 5987 9820 6063 9808 Z M 8060 9680 C 8060 9680 8285 9211 8335 8965 C 8429 8497 8435 7764 8348 7230 C 8264 6707 8090 6270 7707 5610 C 7490 5238 7153 4748 6946 4505 C 6481 3958 5998 3541 5472 3230 C 5367 3169 5346 3160 5335 3171 C 5319 3187 5000 3782 5000 3800 C 5000 3807 7817.247 9800 7817.247 9800 L 8060 9680 Z"/>  </g></svg>),
-    template: [ new Psalm({ type: 'psalm', style: 'psalm', slug: 'psalm_', lookup: { type: 'slug' }, version: { preference: "pssalterVersion" } }) ],
+    template: [ new Psalm({ type: 'psalm', style: 'psalm', slug: 'psalm_', lookup: { type: 'slug' }, version: { preference: "psalterVersion" } }) ],
   },
   {
     label: 'psalter',
