7ab9a29da872905ec940e1535977632cbcbf8b25
Greg Johnston <greg.johnston@gmail.com>
2021-03-22 11:05:10 -0400

    Groundwork for Firebase iOS/Android

--- app/android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ app/android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.baumblatt.capacitor.firebase.auth.CapacitorFirebaseAuth;
+import com.getcapacitor.community.firebaseanalytics.FirebaseAnalytics;
 
 import java.util.ArrayList;
 
@@ -16,6 +19,8 @@ public class MainActivity extends BridgeActivity {
     this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
+      add(FirebaseAnalytics.class);
+      add(CapacitorFirebaseAuth.class);
     }});
   }
 }
