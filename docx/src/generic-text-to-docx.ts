0fd0ccd59f66864bdf582d29abc34ca00f858422
Greg Johnston <greg.johnston@gmail.com>
2021-04-07 18:05:20 -0400

    Fix missing period at end of 'Amen.' in readings

--- docx/src/generic-text-to-docx.ts
+++ docx/src/generic-text-to-docx.ts
@@ -6,18 +6,45 @@ import { DocxChild, docxChildrenFromLDF } from "./ldf-to-docx";
 import { LocaleStrings } from "./locale-strings";
 import { processText } from "./process-text";
 
-export function genericTextToDocx(doc : Refrain | Text | Rubric, style : LDFStyles, displaySettings : DisplaySettings, localeStrings : LocaleStrings) : DocxChild[] {
-  const heading = doc.label || doc.citation
-      ? docxChildrenFromLDF(new Heading({ type: 'heading', metadata: { level: 3 }, value: [doc.label], citation: doc.citation}), displaySettings, localeStrings)
-      : [],
-    response = (doc.metadata?.response || doc.style == 'prayer') && !doc.metadata?.omit_response
-      ? [new TextRun(' '), new TextRun({ style: LDFStyles.Response, text: doc.metadata?.response || localeStrings.amen })]
-      : [];
-  
-  return (doc.value || []).map((p, ii) => new Paragraph({
-    style,
-    children: ii == doc.value.length - 1
-      ? processText(p).concat(response)
-      : processText(p)
-  }));
+export function genericTextToDocx(
+  doc: Refrain | Text | Rubric,
+  style: LDFStyles,
+  displaySettings: DisplaySettings,
+  localeStrings: LocaleStrings
+): DocxChild[] {
+  const heading =
+      doc.label || doc.citation
+        ? docxChildrenFromLDF(
+            new Heading({
+              type: "heading",
+              metadata: { level: 3 },
+              value: [doc.label],
+              citation: doc.citation,
+            }),
+            displaySettings,
+            localeStrings
+          )
+        : [],
+    response =
+      (doc.metadata?.response || doc.style == "prayer") &&
+      !doc.metadata?.omit_response
+        ? [
+            new TextRun(" "),
+            new TextRun({
+              style: LDFStyles.Response,
+              text: doc.metadata?.response ?? localeStrings.amen,
+            }),
+          ]
+        : [];
+
+  return (doc.value || []).map(
+    (p, ii) =>
+      new Paragraph({
+        style,
+        children:
+          ii == doc.value.length - 1
+            ? processText(p).concat(response)
+            : processText(p),
+      })
+  );
 }
