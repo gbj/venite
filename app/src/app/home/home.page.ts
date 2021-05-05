b7adcfefd8b6bd74089d0f701e7dbb970dc9891e
Greg Johnston <greg.johnston@gmail.com>
2021-05-05 14:43:42 -0400

    Remove unused imports

--- app/src/app/home/home.page.ts
+++ app/src/app/home/home.page.ts
@@ -1,33 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
-
-import { BehaviorSubject, Observable, Subject, combineLatest } from "rxjs";
-import { map, tap, switchMap } from "rxjs/operators";
-
-import {
-  HolyDay,
-  Kalendar,
-  LiturgicalDocument,
-  LiturgicalDay,
-  LiturgicalWeek,
-  LiturgicalWeekIndex,
-  ProperLiturgy,
-  ClientPreferences,
-  liturgicalWeek,
-  liturgicalDay,
-  addOneDay,
-  dateToYMD,
-  Liturgy,
-  Preference,
-} from "@venite/ldf";
-
-import { AuthService } from "../auth/auth.service";
-import { CalendarService } from "../services/calendar.service";
-import { DocumentService } from "../services/document.service";
-import { PreferencesService } from "../preferences/preferences.service";
-import { LectionaryService } from "../services/lectionary.service";
-import { AlertController } from "@ionic/angular";
-import { TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: "venite-home",
