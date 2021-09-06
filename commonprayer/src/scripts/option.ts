29598fb16d8e899562ce6e6589d94b2879a6c945
Greg Johnston <greg.johnston@gmail.com>
2021-09-06 12:22:15 -0400

    Correct the way Option selection is being read

--- commonprayer/src/scripts/option.ts
+++ commonprayer/src/scripts/option.ts
@@ -3,11 +3,21 @@ options.forEach((option) => {
   Array.from(option.getElementsByTagName("input")).forEach((input) => {
     input.addEventListener("change", (ev: InputEvent) => {
       const target = ev.target,
-        newSelection = Number((ev.target as HTMLInputElement).value),
+        optionName = (ev.target as HTMLInputElement).name,
+        newSelection =
+          Number(
+            (
+              document.querySelector(
+                `input[type='radio'][name='${optionName}']:checked`
+              ) as HTMLInputElement | undefined
+            )?.value
+          ) ?? 0,
         parent = (target as HTMLElement).closest(".option") as HTMLElement,
         ldf = parent.dataset.ldf,
         doc = ldf ? JSON.parse(decodeURI(ldf)) : null,
         options = parent.querySelectorAll(".options > .doc");
+
+      console.log(ev.target, "\nnewSelection = ", newSelection);
 
       // update selection in data-ldf field
       if (doc) {
