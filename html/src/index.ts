ea73e01e2a3baab0799c364be40b8155361736b1
Greg Johnston <greg.johnston@gmail.com>
2021-10-08 14:23:21 -0400

    Handle Markdown in LDF

--- html/src/index.ts
+++ html/src/index.ts
@@ -22,12 +22,80 @@ import { optionToHTML } from "./option";
 import { LDFToHTMLConfig } from "./config";
 import { parallelToHTML } from "./parallel";
 
+function docToHtml(inDoc: LiturgicalDocument, config: LDFToHTMLConfig): string {
+  const localeStrings = LOCALE_STRINGS["en"];
+
+  switch (inDoc?.type) {
+    case "liturgy":
+      return ((inDoc as Liturgy).value || [])
+        .map((subDoc: LiturgicalDocument) => [
+          subDoc.slug ? `<a name="${subDoc.slug}"></a>` : null,
+          ldfToHTML(subDoc, { ...config, includeLDF: true }),
+        ])
+        .flat()
+        .flat()
+        .filter((n) => Boolean(n))
+        .join("\n");
+    case "parallel":
+      return parallelToHTML(inDoc as Parallel, config);
+    case "option":
+      return optionToHTML(inDoc as Option, config);
+    case "bible-reading":
+      return bibleReadingToHTML(inDoc as BibleReading, localeStrings, config);
+    case "heading":
+      return headingToHTML(inDoc as Heading, localeStrings, config);
+    case "meditation":
+      return "";
+    case "image":
+      return ((inDoc as Image).value || [])
+        .map(
+          (src: string) =>
+            `<article class="doc image"${
+              config.includeLDF
+                ? ` data-ldf="${encodeURI(JSON.stringify(inDoc))}"`
+                : ""
+            }><img src="${src}"></article>`
+        )
+        .join("\n");
+    case "psalm":
+      return psalmToHTML(inDoc as Psalm, localeStrings, config);
+    case "responsive":
+      return responsivePrayerToHTML(
+        inDoc as ResponsivePrayer,
+        localeStrings,
+        config
+      );
+    /** Text fields with various styles */
+    case "refrain":
+      return genericTextToHTML(
+        inDoc as Refrain,
+        "refrain",
+        localeStrings,
+        config
+      );
+    case "rubric":
+      return genericTextToHTML(
+        inDoc as Rubric,
+        "rubric",
+        localeStrings,
+        config
+      );
+    case "text":
+      return genericTextToHTML(
+        inDoc as Text,
+        inDoc?.display_format === "unison" ? "unison" : "normal",
+        localeStrings,
+        config
+      );
+    default:
+      return "";
+  }
+}
+
 export function ldfToHTML(
   inDoc: LiturgicalDocument,
   config: LDFToHTMLConfig
 ): string {
-  const localeStrings = LOCALE_STRINGS["en"];
-
   // lookups by category, slug, lectionary, etc.
   if (inDoc?.lookup && (!inDoc.value || inDoc.value?.length === 0)) {
     return `<article class="lookup ${inDoc.lookup.type} ${
@@ -38,71 +106,10 @@ export function ldfToHTML(
   }
   // compiled documents
   else {
-    switch (inDoc?.type) {
-      case "liturgy":
-        return ((inDoc as Liturgy).value || [])
-          .map((subDoc: LiturgicalDocument) => [
-            subDoc.slug ? `<a name="${subDoc.slug}"></a>` : null,
-            ldfToHTML(subDoc, { ...config, includeLDF: true }),
-          ])
-          .flat()
-          .flat()
-          .filter((n) => Boolean(n))
-          .join("\n");
-      case "parallel":
-        return parallelToHTML(inDoc as Parallel, config);
-      case "option":
-        return optionToHTML(inDoc as Option, config);
-      case "bible-reading":
-        return bibleReadingToHTML(inDoc as BibleReading, localeStrings, config);
-      case "heading":
-        return headingToHTML(inDoc as Heading, localeStrings, config);
-      case "meditation":
-        return "";
-      case "image":
-        return ((inDoc as Image).value || [])
-          .map(
-            (src: string) =>
-              `<article class="doc image"${
-                config.includeLDF
-                  ? ` data-ldf="${encodeURI(JSON.stringify(inDoc))}"`
-                  : ""
-              }><img src="${src}"></article>`
-          )
-          .join("\n");
-      case "psalm":
-        return psalmToHTML(inDoc as Psalm, localeStrings, config);
-      case "responsive":
-        return responsivePrayerToHTML(
-          inDoc as ResponsivePrayer,
-          localeStrings,
-          config
-        );
-      /** Text fields with various styles */
-      case "refrain":
-        return genericTextToHTML(
-          inDoc as Refrain,
-          "refrain",
-          localeStrings,
-          config
-        );
-      case "rubric":
-        return genericTextToHTML(
-          inDoc as Rubric,
-          "rubric",
-          localeStrings,
-          config
-        );
-      case "text":
-        return genericTextToHTML(
-          inDoc as Text,
-          inDoc?.display_format === "unison" ? "unison" : "normal",
-          localeStrings,
-          config
-        );
-      default:
-        return "";
-      //throw new Error(`"${inDoc.type}" is not a valid LDF type.`);
-    }
+    const doc = docToHtml(inDoc, config);
+    // Very limited Markdown
+    return doc
+      .replace(/\*\*([^\*>]*)\*\*/g, "<strong>$1</strong>")
+      .replace(/\*([^\*>]*)\*/g, "<em>$1</em>");
   }
 }
