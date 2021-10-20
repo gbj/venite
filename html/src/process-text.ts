36e617e41262bc3f325fbe5f919438a731abf8c5
Greg Johnston <greg.johnston@gmail.com>
2021-10-20 10:32:20 -0400

    Common Prayer HTML fixes

--- html/src/process-text.ts
+++ html/src/process-text.ts
@@ -1,7 +1,7 @@
 export function processText(text: string): string {
   return text
-    .replace(/\n/g, "<br>")
-    .replace(/\t/g, "    ")
-    .replace(/\*\*([^\*]+)\*\*/g, "<strong>$1</strong>")
-    .replace(/\*([^\*]+)\*/g, "<em>$1</em>");
+    ?.replace(/\n/g, "<br>")
+    ?.replace(/\t/g, "    ")
+    ?.replace(/\*\*([^\*]+)\*\*/g, "<strong>$1</strong>")
+    ?.replace(/\*([^\*]+)\*/g, "<em>$1</em>");
 }
