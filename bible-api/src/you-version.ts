f2bef871bd089cc1fa6ca81c091d30dfea763791
Greg Johnston <greg.johnston@gmail.com>
2023-10-27 09:23:30 -0400

    fix YouVersion

--- bible-api/src/you-version.ts
+++ bible-api/src/you-version.ts
@@ -8,7 +8,7 @@ export async function getYouVersionBook(
 ): Promise<[string, string][]> {
   const url = `https://www.bible.com/bible/${bibleNumber}/${bookCode}.${chapter}.${bibleCode}`,
     page = await requestHTML(url);
-  return Array.from(page.querySelectorAll("span"))
+  const nodes = Array.from(page.querySelectorAll("span"))
     .filter((span) => (span.getAttribute("class") || "").includes("_verse_"))
     .map((node) => {
       let spans = Array.from(node.querySelectorAll("span"));
@@ -23,6 +23,19 @@ export async function getYouVersionBook(
         }
       }
       return [label.replace(/#/g, ""), content] as [string, string];
-    })
-    .filter(([label, content]) => label && content);
+    });
+  const finalNodes = [] as [string, string][];
+  for (const [label, text] of nodes) {
+    if (label && text) {
+      finalNodes.push([label, text]);
+    } else if (text) {
+      const prevNode = finalNodes[finalNodes.length - 1];
+      if (prevNode) {
+        prevNode[1] += `\n${text}`;
+      } else {
+        finalNodes.push([label, text]);
+      }
+    }
+  }
+  return finalNodes;
 }
